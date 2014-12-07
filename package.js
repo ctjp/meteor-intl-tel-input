72eb6d065d1df3ea4040423cbe98c0c8b910159d
ctjp <ctjpit@gmail.com>
2014-12-07 22:51:49 +0800

    Updated packages.js

--- package.js
+++ package.js
@@ -1,15 +1,19 @@
 Package.describe({
   name: 'ctjp:meteor-intl-tel-input',
   summary: 'Meteor package for https://github.com/Bluefieldscom/intl-tel-input',
-  version: '3.7.1_1',
+  version: '3.7.1_2',
   git: 'https://github.com/ctjp/meteor-intl-tel-input.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  
+
+  //== Use
+  // Client only
   api.use('jquery', 'client');
-  
+
+  //== Add files
+  // Client only
   api.addFiles([
     'lib/intl-tel-input/build/js/intlTelInput.js',
     'lib/intl-tel-input/build/css/intlTelInput.css',
@@ -18,10 +22,14 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
+  //== Use
+  // Client only
   api.use([
     'tinytest',
     'ctjp:meteor-intl-tel-input'
   ], 'client');
 
+  //== Add files
+  // Client only
   api.addFiles('tests/meteor-intl-tel-input.js', 'client');
 });
